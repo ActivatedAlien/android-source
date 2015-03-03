dab89fc4dedc72dc45b13e726a058df09a982895
Ankit Buddhiraju <zaxayon@gmail.com>
2015-03-02 22:22:03 -0500

    Checkpoint 35 assignment completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,6 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
@@ -11,12 +12,17 @@ import io.bloc.android.blocly.R;
  * Created by theinnformaster on 3/2/15.
  */
 public class BloclyActivity extends Activity {
+    private TextView mWelcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-        Toast.makeText(this,
-                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                Toast.LENGTH_LONG).show();
+
+        String fakeFeedName = BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle();
+
+        mWelcomeText = (TextView) findViewById(R.id.welcome_text);
+        mWelcomeText.setText(fakeFeedName);
+        Toast.makeText(this, fakeFeedName, Toast.LENGTH_LONG).show();
     }
 }
