820ff6ddf171e7ea59c9dc45bb2fad76d26b19bb
Ankit Buddhiraju <zaxayon@gmail.com>
2015-03-08 21:50:04 -0400

    Checkpoint 50 assignment completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
@@ -2,15 +2,22 @@ package io.bloc.android.blocly.api.network;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * Created by theinnformaster on 3/5/15.
  */
 public class GetFeedsNetworkRequest extends NetworkRequest {
+    private static final String XML_TAG_ITEM = "item";
 
     String [] feedUrls;
 
@@ -26,18 +33,27 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
                 return null;
             }
             try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line = bufferedReader.readLine();
-                while (line != null) {
-                    Log.v(getClass().getSimpleName(), "Line: " + line);
-                    line = bufferedReader.readLine();
-                }
-                bufferedReader.close();
+                DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document xmlDocument = documentBuilder.parse(inputStream);
+                NodeList allItemNodes = xmlDocument.getElementsByTagName(XML_TAG_ITEM);
+                Log.v("Number of Items", allItemNodes.getLength() + "");
             } catch (IOException e) {
                 e.printStackTrace();
                 setErrorCode(ERROR_IO);
                 return null;
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
             }
+        }
+        return null;
+    }
+
+    private String optFirstTagFromDocument(Document document, String tagName) {
+        NodeList elementsByTagName = document.getElementsByTagName(tagName);
+        if (elementsByTagName.getLength() > 0) {
+            return elementsByTagName.item(0).getTextContent();
         }
         return null;
     }
